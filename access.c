67e366d6bdb8ea9f6df1404c7d5bab5dfd39d2db
ceecurvin <ccurvi1@lsu.edu>
2018-08-24 19:41:28 +0000

    didnt really change anything

--- access.c
+++ access.c
@@ -20,58 +20,19 @@ int main(int ac, char **argv, char **env)
 	pid_t process;
 
 	do {
+		checks = getinput();
 
-		checks = getinput(/*&buffy, &size, stdin*/);
-
-        /*input stands for command entered
-          if input is found, execute*/
 	printf("before access\n");
+
         if ((access(checks[0], X_OK) == 0))
         {
 		forks = forkitfunction(checks);
         }
         else
         {
-                /*pt equals array of paths 
-                pt = path(env);
-                /* loop through array of path
-                while(pt[count])
-                {
-                        /*if path exists then execute(pathname, token, null)
-
-                         append = _strcat(pt[count], checks[0]);
-			printf("append %s\n", append);
-                        if (access(append, X_OK) == 0)
-                        {
-                                process = fork();
-
-				if (process == 0)
-				{
-
-					if (execve(append, checks, env) == -1)
-						perror("Error");
-					else
-						return(0);
-				}
-				else
-				{
-					wait(&status);
-					printf("winner: %s\n", append);
-					free(append);
-					append = NULL;
-					break;
-				}
-		        }
-			free(append);
-			append = NULL;
-                        count++;
-                }
-		count = 0;
-        free(pt[0]);
-	free(pt);*/
-
 		paths = pathfork(checks, env);
 	}
+
 	free(checks[0]);
 	free(checks);
 
